074cd56084ec32638b5ef79eff4d2a1be6f3cffe
SwagMaverick <ermeshka22@gmail.com>
2026-04-16 11:11:30 +0500

    switch to mockapi for persistent storage

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,14 +1,14 @@
 import 'package:dio/dio.dart';
 import '../models/item.dart';
 
-// using jsonplaceholder as a free fake api for testing
+// using mockapi.io for persistent fake data
 class ApiService {
-  final Dio _dio = Dio(BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com'));
+  final Dio _dio = Dio(BaseOptions(baseUrl: 'https://69e07c7029c070e6597b8a17.mockapi.io'));
 
   Future<List<Item>> getItems() async {
     try {
       // limit to 20 so the list isn't massive
-      final response = await _dio.get('/todos', queryParameters: {'_limit': 20});
+      final response = await _dio.get('/items', queryParameters: {'limit': 20});
       final data = response.data as List<dynamic>;
       return data
           .map((json) => Item.fromJson(json as Map<String, dynamic>))
@@ -20,7 +20,7 @@ class ApiService {
 
   Future<Item> createItem(String title) async {
     try {
-      final response = await _dio.post('/todos', data: {
+      final response = await _dio.post('/items', data: {
         'title': title,
         'createdAt': DateTime.now().toIso8601String(),
       });
@@ -32,7 +32,7 @@ class ApiService {
 
   Future<void> deleteItem(String id) async {
     try {
-      await _dio.delete('/todos/$id');
+      await _dio.delete('/items/$id');
     } on DioException catch (e) {
       throw Exception('deleteItem error: ${e.message}');
     }
